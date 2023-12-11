2cd17cb55aeddb5cc1a0a02b498c560769a18ceb
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2023-12-11 09:03:48 +0200

    button-step-count-date-message

--- src/App.js
+++ src/App.js
@@ -11,13 +11,32 @@ export default function App() {
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const[step, setStep] = useState(1);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  console.log(date);
   return (
     <div>
       <div>
-        <button onClick={()=> setCount(c=> c-1)}>-</button>
-        <span>Count: {count}</span>       
-        <button onClick={()=> setCount(c=> c+1)}>+</button>
+        <div>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+        </div>
+     
+
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
+      <p>
+        <span>
+          {count === 0 ? "Today is " : count > 0 ? `${count} 
+          days from today is ` : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
   );
 }
